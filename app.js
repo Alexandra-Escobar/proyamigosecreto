dbbc68406e90d9eb482f928e7cf7d7a41eecd9c6
Alexandra-Escobar <alexandraescobarcv@gmail.com>
2025-03-18 11:58:07 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = ["Juan", "Aby", "Ale", "Jose", "Pedro"];
+
+document.getElementById (amigos)
+alert = "Por Favor, inserte un nombre"
+PushManager(amigos)
+clear ( )
+
+lista.innerHTML = "amigos"
+<li>["Juan"] 
+<li> ["Aby"]
+<li> ["Ale"]
+<li> ["Jose"]
+<li> ["Pedro"]     
+
+Math.random(agregarAmigo)
+Math.floor(sortearAmigo)
+
+document.getElementById(amigos)
+
+
+//2
